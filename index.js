b1bb0a712e9ed18a5bfefbd07004f6310bae81fa
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2021-07-29 08:11:17 -0400

    basic requirements completed

---
+++ index.js
@@ -0,0 +1,124 @@
+var getTasks = function() {
+  $.ajax({
+    type: "GET",
+    url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=117",
+    dataType: "json",
+    success: function (response, textStatus) {
+      $('.to-dos').empty();
+      console.log(response.tasks);
+      response.tasks.forEach(function (task) {
+        var taskContent = `
+          <div class="to-do">
+            <div class="left-side">
+              <button class="select" data-id="${task.id}" data-completed="${task.completed}">
+                <ion-icon name=${task.completed ? "checkmark-circle-outline" : "ellipse-outline"} size="large"></ion-icon>
+              </button>
+              <p class=${task.completed ? "task-content-completed" : "task-content"}>${task.content}</p>
+            </div>
+            <button class="delete" data-id="${task.id}"><ion-icon name="close-outline" size="large"></ion-icon></button>
+          </div>
+        `;
+        $('.to-dos').append(taskContent);
+      });
+
+      // update the amount of task items
+      $('.to-do-amount span').text(response.tasks.length);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+var submitTask = function () {
+
+  var taskInput = $('.new-to-do input');
+  
+  $.ajax({
+    type: "POST",
+    url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=117",
+    contentType: 'application/json',
+    data: JSON.stringify({
+      task: {
+        content: taskInput.val()
+      }
+    }),
+    dataType: "json",
+    success: function (response, textStatus) {
+      console.log(response);
+      getTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+
+  taskInput.val('');  
+
+};
+
+var deleteTask = function (id) {
+  $.ajax({
+    type: "DELETE",
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=117`,
+    success: function (response, textStatus) {
+      console.log(response);
+      getTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+};
+
+var taskIsComplete = function(id) {
+  $.ajax({
+    type: 'PUT',
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_complete?api_key=117`,
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+      getTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  }); 
+} 
+
+var taskIsActive = function(id) {
+  $.ajax({
+    type: 'PUT',
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_active?api_key=117`,
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+      getTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  }); 
+}
+
+$(document).ready(function () {
+
+  getTasks();
+
+  $('.new-to-do').on('submit', function (event) {
+    event.preventDefault();
+    submitTask();
+  });
+
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
+  
+  $(document).on('click', '.select', function () {
+    if ($(this).data('completed')) {
+      taskIsActive($(this).data('id'));
+    } else {
+    } taskIsComplete($(this).data('id'));
+  });
+
+});
